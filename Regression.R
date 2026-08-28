ab92cc7acdfa30e0be73d8f68a1a4508c0c4dac8
Pawar <pawar1550@gmail.com>
2026-08-28 13:18:43 +0530

    Add files via upload

--- Regression.R
+++ Regression.R
@@ -6,3 +6,27 @@ training_data <- mtcars
 # Plot the data as a scatter plot
 scatter.smooth(x= training_data$disp, y= training_data$wt, main="SpeedVSdistance")
 
+# 50% split--Underfitting
+# 70% split-----------------Assignment
+# 90% split--Overfitting-----------------Assignment
+
+# Create a training dataset with 50% split
+training_dataset <- training_data[1:16,]
+test_dataset <- training_data[17:32,]
+
+# Creating regression model on mpg and wt of car
+regression_model <- lm(mpg ~ wt, data=training_dataset)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
